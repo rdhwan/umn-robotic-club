d44df360832ddc5ffd91386d4658f4451e8dbc47
rdhwan <fathanridhwan@gmail.com>
2022-11-12 11:55:49 +0700

    optimizing code

--- color_detection/detector.py
+++ color_detection/detector.py
@@ -1,45 +1,49 @@
+"""Color finder with opencv"""
+
 import cv2
 import numpy as np
 
 
-# frame = cv2.imread("img/traffic_light.png")
-
-
-def detect_green_color(frame: cv2.Mat) -> cv2.Mat:
+def detect_rgb(
+    frame: cv2.Mat,
+    red_low: list[int],
+    red_hi: list[int],
+    green_low: list[int],
+    green_hi: list[int],
+    blue_low: list[int],
+    blue_hi: list[int],
+) -> cv2.Mat:
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    red_lower = np.array([140, 166, 58], np.uint8)
-    red_upper = np.array([179, 255, 255], np.uint8)
-    red_mask = cv2.inRange(hsv_frame, red_lower, red_upper)
-
-    green_lower = np.array([36, 88, 83], np.uint8)
-    green_upper = np.array([68, 255, 255], np.uint8)
-    green_mask = cv2.inRange(hsv_frame, green_lower, green_upper)
-
-    blue_lower = np.array([93, 60, 118], np.uint8)
-    blue_upper = np.array([144, 255, 255], np.uint8)
-    blue_mask = cv2.inRange(hsv_frame, blue_lower, blue_upper)
+    red_mask = cv2.inRange(
+        hsv_frame, np.array(red_low, np.uint8), np.array(red_hi, np.uint8)
+    )
+    green_mask = cv2.inRange(
+        hsv_frame, np.array(green_low, np.uint8), np.array(green_hi, np.uint8)
+    )
+    blue_mask = cv2.inRange(
+        hsv_frame, np.array(blue_low, np.uint8), np.array(blue_hi, np.uint8)
+    )
 
     kernel = np.ones((5, 5), "uint8")
 
-    # For red color
+    # Dilating
     red_mask = cv2.dilate(red_mask, kernel)
-    res_red = cv2.bitwise_and(frame, frame, mask=red_mask)
-
-    # For green color
     green_mask = cv2.dilate(green_mask, kernel)
-    res_green = cv2.bitwise_and(frame, frame, mask=green_mask)
-
-    # For blue color
     blue_mask = cv2.dilate(blue_mask, kernel)
-    res_blue = cv2.bitwise_and(frame, frame, mask=blue_mask)
+
+    # NOTE: This remains unused.
+    # and bitwise
+    # res_red = cv2.bitwise_and(frame, frame, mask=red_mask)
+    # res_green = cv2.bitwise_and(frame, frame, mask=green_mask)
+    # res_blue = cv2.bitwise_and(frame, frame, mask=blue_mask)
 
     # Creating contour to track red color
     contours, hierarchy = cv2.findContours(
         red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
     )
 
-    for pic, contour in enumerate(contours):
+    for contour in contours:
         area = cv2.contourArea(contour)
         if area > 300:
             x, y, w, h = cv2.boundingRect(contour)
@@ -54,7 +58,7 @@ def detect_green_color(frame: cv2.Mat) -> cv2.Mat:
         green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
     )
 
-    for pic, contour in enumerate(contours):
+    for contour in contours:
         area = cv2.contourArea(contour)
         if area > 300:
             x, y, w, h = cv2.boundingRect(contour)
@@ -74,7 +78,7 @@ def detect_green_color(frame: cv2.Mat) -> cv2.Mat:
         blue_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
     )
 
-    for pic, contour in enumerate(contours):
+    for contour in contours:
         area = cv2.contourArea(contour)
         if area > 300:
             x, y, w, h = cv2.boundingRect(contour)
@@ -90,25 +94,3 @@ def detect_green_color(frame: cv2.Mat) -> cv2.Mat:
             )
 
     return frame
-
-
-#
-# # Program Termination
-# while True:
-# cv2.imshow("Sipaling robotic", frame)
-# if cv2.waitKey(1) & 0xFF == ord("q"):
-# cv2.destroyAllWindows()
-# break
-
-cap = cv2.VideoCapture(0)
-
-while True:
-    _, frame = cap.read()
-
-    processed = detect_green_color(frame=frame)
-    cv2.imshow("Capt", processed)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
