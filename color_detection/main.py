545fc47da0485e59daf11efbaa3cd064a3ba698a
rdhwan <fathanridhwan@gmail.com>
2022-11-24 12:52:32 +0700

    testing for ms

--- color_detection/main.py
+++ color_detection/main.py
@@ -14,39 +14,47 @@ BLUE_LOW = [95, 66, 109]
 BLUE_HIGH = [130, 255, 255]
 
 
-# detect color on single frame
-frame = cv2.imread("img/sample_blue.jpg")
+# # detect color on single frame
+# frame = cv2.imread("img/sample_blue.jpg")
 
-start = time.perf_counter()
-processed = detector.detect_rgb(
-    frame=frame,
-    red_low=RED_LOW,
-    red_hi=RED_HIGH,
-    green_low=GREEN_LOW,
-    green_hi=GREEN_HIGH,
-    blue_low=BLUE_LOW,
-    blue_hi=BLUE_HIGH,
-)
-end = time.perf_counter()
+# start = time.perf_counter()
+# processed = detector.detect_rgb(
+#     frame=frame,
+#     red_low=RED_LOW,
+#     red_hi=RED_HIGH,
+#     green_low=GREEN_LOW,
+#     green_hi=GREEN_HIGH,
+#     blue_low=BLUE_LOW,
+#     blue_hi=BLUE_HIGH,
+# )
+# end = time.perf_counter()
 
-print(f"Process took: {end - start}")
-
-while True:
-    cv2.imshow("sample", processed)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-
-# # using VideoCapture to get video feeds
-# cap = cv2.VideoCapture(0)
+# print(f"Process took: {end - start}")
 
 # while True:
-#     _, frame = cap.read()
-
-#     processed = detector.detect_rgb(frame=frame)
-#     cv2.imshow("Capt", processed)
+#     cv2.imshow("sample", processed)
 #     if cv2.waitKey(1) & 0xFF == ord("q"):
 #         break
 
-# cap.release()
+# using VideoCapture to get video feeds
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    processed = detector.detect_rgb(
+        frame=frame,
+        red_low=RED_LOW,
+        red_hi=RED_HIGH,
+        green_low=GREEN_LOW,
+        green_hi=GREEN_HIGH,
+        blue_low=BLUE_LOW,
+        blue_hi=BLUE_HIGH,
+    )
+
+    cv2.imshow("Capt", processed)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
 
 cv2.destroyAllWindows()
